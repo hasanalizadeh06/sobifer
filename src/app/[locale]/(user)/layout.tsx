b69e16a0a33d548f585d2d8fcb8ddf37097361f6
HasanAlizadeh <texnovideo181@gmail.com>
2025-08-29 16:26:24 +0400

    test

--- src/app/[locale]/(user)/layout.tsx
+++ src/app/[locale]/(user)/layout.tsx
@@ -1,16 +1,22 @@
 import React, { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { usePathname } from "next/navigation";
 interface UserLayoutProps {
   children: ReactNode;
 }
 
+const HIDDEN_LAYOUT_PATHS = ["/login", "/register", "/forgot-password"];
+
 const UserLayout = ({ children }: UserLayoutProps) => {
+  const pathname = usePathname();
+  const hideLayout = HIDDEN_LAYOUT_PATHS.includes(pathname);
+
   return (
     <div className="bg-white">
-      <Navbar />
+      {!hideLayout && <Navbar />}
       <div>{children}</div>
-      <Footer />
+      {!hideLayout && <Footer />}
     </div>
   );
 };
